6a95eac66bf83a4579418574374c9adef0e23c35
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-12-11 19:49:17 +0100

    Task completed

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -16,6 +16,8 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
+#        if (i % 3) == 0:
+#            tmp_result.append("Fizz")
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
